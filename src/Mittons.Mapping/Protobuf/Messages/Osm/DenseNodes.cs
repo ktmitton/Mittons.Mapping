d751477eebbdc82b2ebdf11fc339f7a190920839
ktmitton <ktmitton@gmail.com>
2026-04-07 22:09:04 -0400

    Removed code that wasn't used

--- src/Mittons.Mapping/Protobuf/Messages/Osm/DenseNodes.cs
+++ src/Mittons.Mapping/Protobuf/Messages/Osm/DenseNodes.cs
@@ -2,64 +2,13 @@ using Mittons.Mapping.Extensions;
 
 namespace Mittons.Mapping.Protobuf.Messages.Osm;
 
-public class DenseNodes : IEquatable<DenseNodes>
+public class DenseNodes
 {
-    public long[] Ids { get; init; } = [];
-    public Info[]? Infos { get; init; }
-    public long[] Latitudes { get; init; } = [];
-    public long[] Longitudes { get; init; } = [];
-    public int[] KeyValues { get; init; } = [];
-
     public const byte IdFieldNumber = 1;
     public const byte DenseInfoFieldNumber = 5;
     public const byte LatitudeFieldNumber = 8;
     public const byte LongitudeFieldNumber = 9;
     public const byte KeyValueFieldNumber = 10;
-
-    public DenseNodes() { }
-
-    public DenseNodes(Memory<byte> source)
-    {
-        throw new NotImplementedException();
-    }
-
-    public bool Equals(DenseNodes? other)
-    {
-        if (other is null) return false;
-        if (ReferenceEquals(this, other)) return true;
-
-        return Ids.SequenceEqual(other.Ids)
-            && Latitudes.SequenceEqual(other.Latitudes)
-            && Longitudes.SequenceEqual(other.Longitudes)
-            && KeyValues.SequenceEqual(other.KeyValues)
-            && (Infos ?? []).SequenceEqual(other?.Infos ?? []);
-    }
-
-    public override bool Equals(object? obj) => Equals(obj as DenseNodes);
-    public static bool operator ==(DenseNodes? left, DenseNodes? right) => Equals(left, right);
-    public static bool operator !=(DenseNodes? left, DenseNodes? right) => !Equals(left, right);
-
-    public override int GetHashCode()
-    {
-        HashCode hash = new();
-
-        foreach (var id in Ids)
-            hash.Add(id);
-
-        foreach (var latitude in Latitudes)
-            hash.Add(latitude);
-
-        foreach (var longitude in Longitudes)
-            hash.Add(longitude);
-
-        foreach (var key in KeyValues)
-            hash.Add(key);
-
-        foreach (var info in Infos ?? [])
-            hash.Add(info);
-
-        return hash.ToHashCode();
-    }
 }
 
 internal static class DenseNodesMemoryExtensions
--- test/Mittons.Mapping.Tests/Protobuf/Messages/Osm/DenseNodesTests.cs
+++ test/Mittons.Mapping.Tests/Protobuf/Messages/Osm/DenseNodesTests.cs
@@ -6,40 +6,6 @@ namespace Mittons.Mapping.Tests.Protobuf.Messages.Osm;
 public class DenseNodesTests
 {
     [Test]
-    [DenseNodesEqualityData]
-    public async Task EqualityTests(DenseNodes left, DenseNodes right)
-    {
-        // Arrange
-
-        // Act
-        var actualEqualityFunctionResult = left.Equals(right);
-        var actualEqualityOperatorResult = left == right;
-        var actualInequalityOperatorResult = left != right;
-
-        // Assert
-        await Assert.That(actualEqualityFunctionResult).IsTrue();
-        await Assert.That(actualEqualityOperatorResult).IsTrue();
-        await Assert.That(actualInequalityOperatorResult).IsFalse();
-    }
-
-    [Test]
-    [DenseNodesInequalityData]
-    public async Task InequalityTests(DenseNodes left, DenseNodes right)
-    {
-        // Arrange
-
-        // Act
-        var actualEqualityFunctionResult = left.Equals(right);
-        var actualEqualityOperatorResult = left == right;
-        var actualInequalityOperatorResult = left != right;
-
-        // Assert
-        await Assert.That(actualEqualityFunctionResult).IsFalse();
-        await Assert.That(actualEqualityOperatorResult).IsFalse();
-        await Assert.That(actualInequalityOperatorResult).IsTrue();
-    }
-
-    [Test]
     [DenseNodesFromMemoryData]
     public async Task ReadDenseNodeTests(byte[] source, Node[] expectedNodes)
     {
